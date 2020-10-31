d432b0df83894beb2138347a9c241ced27ccc8d3
pczuppon <65686356+pczuppon@users.noreply.github.com>
2020-10-31 17:43:08 +0100

    Fig1b final

--- manuscript/Fig1/b/fig1b.py
+++ manuscript/Fig1/b/fig1b.py
@@ -12,9 +12,9 @@ mu = 10**(-3)
 dt = 0.001
 t = np.arange(0,10+dt,dt)
 
-frac = [0.01,0.015,0.02,0.03,0.04,0.05,0.06,0.08,0.1,0.15,0.2,0.3,0.4,0.6,0.8,1,1.5,2,3,4,6,8,10,15,20,30,40,60,80,100]
+frac = [0.001,0.0015,0.002,0.003,0.004,0.005,0.006,0.008,0.01,0.015,0.02,0.03,0.04,0.05,0.06,0.08,0.1,0.15,0.2,0.3,0.4,0.6,0.8,1,1.5,2,3,4,6,8,10,15,20,30,40,60,80,100]
 ################# Contour lines time to viral peak
-days = [4,5,6,7,8,9]
+days = [3,4,5,6,7,8,9]
 res_days = np.zeros((len(days),len(frac)))
 
 l, j = 0, 0
@@ -142,7 +142,11 @@ for l in range(len(load)):
 prop = np.asarray(frac)/100
 
 for i in range(len(days)):
-    plt.semilogx(prop,res_days[i,:],linewidth=2,color='C0')
+    if (max(res_days[i,:])>18.7):
+        ind_max = min(np.where(res_days[i,:]>18.7)[0])-1
+    else:
+        ind_max = len(res_days[i,:])
+    plt.semilogx(prop[0:ind_max],res_days[i,0:ind_max],linewidth=2,color='C0')
 
 for i in range(len(load)):
     if (min(res_load[i,:])<18.7):
@@ -151,8 +155,8 @@ for i in range(len(load)):
     else:
         ind_min = 0
         ind_max = len(res_load[i,:])
-    if (i == 1):
-    	plt.plot(prop[ind_min:ind_max+1],res_load[i,ind_min:ind_max+1],linewidth=2,color='C1')
+    if (i == 1 or i==0):
+        plt.plot(prop[ind_min:ind_max+1],res_load[i,ind_min:ind_max+1],linewidth=2,color='C1')
     else:
     	plt.plot(prop[ind_min-1:ind_max+1],res_load[i,ind_min-1:ind_max+1],linewidth=2,color='C1')
 
@@ -213,7 +217,7 @@ while (update == 0):
             else:
                 R0 += 0.1
 
-plt.plot(frac_France/100,res_France,marker="o",color="gray",markersize=10)
+#plt.plot(frac_France/100,res_France,marker="o",color="gray",markersize=10)
 
 ################# Singapore
 R0 = 3
@@ -322,7 +326,126 @@ while (update == 0):
             else:
                 R0 += 0.1
 
-plt.plot(frac_Ger/100,res_Ger,marker="o",color="lightgrey",markersize=10)
+plt.plot(frac_Ger/100,res_Ger,marker="o",color="gray",markersize=10)
+
+################ add data points
+######### Hongkong
+t = np.arange(0,7.5+dt,dt)
+
+R0 = 3
+frac_Hongkong = 10**(-2)
+update = 0
+while (update == 0):
+    print(R0)
+    beta = c *delta*R0/((p*mu-delta*R0)*frac_Hongkong*T0/100)
+    #
+    ################# ODE Simulation
+    ################# Initial condition
+    V0 = 1
+    E0 = 0
+    I0 = 0
+    NV0 = 0
+    T0_upd = T0*frac_Hongkong/100
+    #
+    ################# Trajectories
+    Tfull, Efull, Ifull, Vfull, NVfull = [], [], [], [], []
+    #
+    Tfull.append(T0_upd)
+    Efull.append(E0)
+    Ifull.append(I0)
+    Vfull.append(V0)
+    NVfull.append(NV0)
+    #
+    i = 0
+    for i in range(len(t)-1):
+        Tfull.append(max(0,Tfull[-1] - beta * Tfull[-1] * Vfull[-1] * dt))
+        Efull.append(max(0,Efull[-1] + beta * Tfull[-1] * Vfull[-1] * dt - k * Efull[-1] * dt))
+        Ifull.append(max(0,Ifull[-1] + k * Efull[-1] *dt - delta * Ifull[-1] * dt))
+        Vfull.append(max(0,Vfull[-1] + mu * p * Ifull[-1] * dt - c * Vfull[-1] * dt - beta * Tfull[-1] * Vfull[-1] * dt))
+        NVfull.append(max(0,NVfull[-1] + (1-mu) * p * Ifull[-1] * dt - c * NVfull[-1] * dt))
+    #
+    Vwork = np.asarray(Vfull)
+    NVwork = np.asarray(NVfull)
+    Vtot = Vwork + NVwork
+    #
+    if (np.max(Vtot)==Vtot[-1] or np.max(Vtot)==Vtot[0]):
+        R0 += 0.1
+    else:
+        tmax = np.where(Vtot == np.max(Vtot))[0][0]*dt
+        if (np.max(Vtot) < 10**6):
+            if (tmax > 4):
+                R0 += 0.1
+            else:
+                frac_Hongkong += 10**(-3)
+                R0 = 3
+        else:
+            if (tmax <= 4):
+                res_Hongkong = R0
+                update = 1
+            else:
+                R0 += 0.1
+
+plt.plot(frac_Hongkong/100,res_Hongkong,marker="o",color="darkgray",markersize=10)
+
+################ add data points
+######### NBA
+t = np.arange(0,7.5+dt,dt)
+
+R0 = 3
+frac_nba = 10**(-3)
+update = 0
+while (update == 0):
+    print(R0)
+    beta = c *delta*R0/((p*mu-delta*R0)*frac_nba*T0/100)
+    #
+    ################# ODE Simulation
+    ################# Initial condition
+    V0 = 1
+    E0 = 0
+    I0 = 0
+    NV0 = 0
+    T0_upd = T0*frac_nba/100
+    #
+    ################# Trajectories
+    Tfull, Efull, Ifull, Vfull, NVfull = [], [], [], [], []
+    #
+    Tfull.append(T0_upd)
+    Efull.append(E0)
+    Ifull.append(I0)
+    Vfull.append(V0)
+    NVfull.append(NV0)
+    #
+    i = 0
+    for i in range(len(t)-1):
+        Tfull.append(max(0,Tfull[-1] - beta * Tfull[-1] * Vfull[-1] * dt))
+        Efull.append(max(0,Efull[-1] + beta * Tfull[-1] * Vfull[-1] * dt - k * Efull[-1] * dt))
+        Ifull.append(max(0,Ifull[-1] + k * Efull[-1] *dt - delta * Ifull[-1] * dt))
+        Vfull.append(max(0,Vfull[-1] + mu * p * Ifull[-1] * dt - c * Vfull[-1] * dt - beta * Tfull[-1] * Vfull[-1] * dt))
+        NVfull.append(max(0,NVfull[-1] + (1-mu) * p * Ifull[-1] * dt - c * NVfull[-1] * dt))
+    #
+    Vwork = np.asarray(Vfull)
+    NVwork = np.asarray(NVfull)
+    Vtot = Vwork + NVwork
+    #
+    if (np.max(Vtot)==Vtot[-1] or np.max(Vtot)==Vtot[0]):
+        R0 += 0.1
+    else:
+        tmax = np.where(Vtot == np.max(Vtot))[0][0]*dt
+        if (np.max(Vtot) < 4*10**5):
+            if (tmax > 3):
+                R0 += 0.1
+            else:
+                frac_nba += 10**(-4)
+                R0 = 3
+        else:
+            if (tmax <= 3):
+                res_nba = R0
+                update = 1
+            else:
+                R0 += 0.1
+
+plt.plot(frac_nba/100,res_nba,marker="o",color="olive",markersize=10)
+
 
 ########################## Finish plot
 plt.ylim((0,20))
